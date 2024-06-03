d1ce2f900ae768e9483891413d055f6a614526b4
Uzzmasaiyed <writeruzzi@gmail.com>
2024-06-03 21:14:27 +0530

    update

--- demo1.js
+++ demo1.js
@@ -4,3 +4,4 @@ const sub = 10 - 2
 console.log("The added value is (" + add + ")")
 console.log("The subtracted value is (" + sub + ")")
 //write node filename to run
+//write npm init to create package.json
