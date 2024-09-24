e4d8130bb498ac8a653364a1d32b3fdd42cb8060
Uzzmasaiyed <writeruzzi@gmail.com>
2024-09-24 12:07:33 +0530

    update

---
+++ chatsystem/demo1.js
@@ -0,0 +1,3 @@
+//write node filename to run
+//write nodemon filename to reload after changes
+//write npm init to create package.json
--- demo1.js
+++
@@ -1,8 +0,0 @@
-
-const add = 10 + 2
-const sub = 10 - 2
-console.log("The added value is (" + add + ")")
-console.log("The subtracted value is (" + sub + ")")
-//write node filename to run
-//write nodemon filename to reload after changes
-//write npm init to create package.json
